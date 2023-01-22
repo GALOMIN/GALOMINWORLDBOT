ce57b20212809c1469174cd42937b1b29d33ef01
GALOMIN <123244294+GALOMIN@users.noreply.github.com>
2023-01-22 18:35:30 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -1,5 +1,9 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config as cfg
+from dotenv import load_dotenv, find_dotenv
+
+# Find .env file
+load_dotenv(find_dotenv())
 
 btnUrlChannel = InlineKeyboardButton(text='Подписаться', url=cfg.CHANNEL_URL)
 channelMenu = InlineKeyboardMarkup(row_width=1)
